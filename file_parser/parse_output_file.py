c4beb32c7099cec3541082bfe74065c61ed3c299
tunczyk101 <jolka2102@gmail.com>
2024-05-11 21:00:22 +0200

    update get_simulation_results

--- file_parser/parse_output_file.py
+++ file_parser/parse_output_file.py
@@ -1,6 +1,32 @@
-def get_simulation_results(filename, as_dictionary=False):
-    nodes = {}
-    links = {}
+NODES_METRIC = "Pressure"
+LINKS_METRIC = "Flow"
+
+
+def find_metric_id(line: str, metric: str) -> int:
+    """
+    Finds the id/number of the given metric in the line.
+
+    :param line: A string representing a line with columns separated by whitespace.
+    :param metric: The name of the metric to search for.
+    :return: The id of the searched column (columns numbers start from 1).
+    """
+    for i, col in enumerate(line.split()):
+        if col == metric:
+            return i + 1
+
+    raise ValueError(f"Metric {metric} not found")
+
+
+def get_simulation_results(filename: str) -> list[float]:
+    """
+    Returns Pressure and Flow results for given file with EPANET output
+
+    :param filename: name of a file with EPANET results
+    :return: list with Pressure and Flow results from given file
+    """
+    results = []
+    nodes_metric_id = None
+    links_metric_id = None
 
     with open(filename) as output_file:
         lines = output_file.readlines()
@@ -8,43 +34,34 @@ def get_simulation_results(filename, as_dictionary=False):
         while i < len(lines):
             line = lines[i].split()
             if len(line) > 0:
+                metric_id = None
                 match line[0]:
-                    case "Node":
-                        # get hour
-                        nodes[line[3]] = {}
-                        i += 5
+                    case "Node":  # Set metric_id for Nodes (Pressure)
+                        if (
+                            nodes_metric_id is None
+                        ):  # if we do not know in which col is Nodes metric - find it
+                            nodes_metric_id = find_metric_id(lines[i + 2], NODES_METRIC)
+                        metric_id = nodes_metric_id
+                    case "Link":  # Set metric_id for Links (Flow)
+                        if (
+                            links_metric_id is None
+                        ):  # if we do not know in which col is Links metric - find it
+                            links_metric_id = find_metric_id(lines[i + 2], LINKS_METRIC)
+                        metric_id = links_metric_id
+
+                if metric_id is not None:  # read all Nodes/Links metrics values
+                    i += 5  # Skip metadata lines
+                    node_line = lines[i].split()
+                    while len(node_line) > 0:
+                        results.append(node_line[metric_id])
+                        i += 1
                         node_line = lines[i].split()
-                        while len(node_line) > 0:
-                            # for each id collect pressure
-                            nodes[line[3]][node_line[0]] = node_line[4]
-                            i += 1
-                            node_line = lines[i].split()
-                        continue
-                    case "Link":
-                        # get hour
-                        links[line[3]] = {}
-                        i += 5
-                        link_line = lines[i].split()
-                        while len(link_line) > 0:
-                            # for each id collect flow
-                            links[line[3]][link_line[0]] = link_line[1]
-                            i += 1
-                            link_line = lines[i].split()
-                        continue
+                    continue  # End of metrics for Nodes/Links
 
             i += 1
 
-    if as_dictionary:
-        return {
-            "nodes": nodes,
-            "links": links
-        }
-
-    results = [value for h in nodes.values() for value in h.values()]
-    results += [value for h in links.values() for value in h.values()]
     return results
 
 
 if __name__ == "__main__":
     print(get_simulation_results("../epanet_model/output.txt"))
-    print(get_simulation_results("../epanet_model/output.txt", as_dictionary=True))
