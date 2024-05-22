596b66701353d1dec37d9cf4c0e14f81ddf31bec
JudytaBakowska <judytabakowska@gmail.com>
2024-05-22 21:44:36 +0200

    update optimalize_epanet

--- optimize_epanet.py
+++ optimize_epanet.py
@@ -1,12 +1,11 @@
 from scipy.optimize import differential_evolution
 import subprocess
 import shutil
-from file_parser.parse_output_file import get_simulation_results
+from parse_output_file import get_simulation_results
 from calculate_error import calculate_error
-import sys
 
-JUNCTIONS_TO_ADJUST = [' SO/ZD', ' SW/SO', ' SW/RO', ' SD/GO1', ' SW/AN', ' HP7']
-PIPES_TO_ADJUST = [' p23', ' p129', ' p159']
+JUNCTIONS_TO_ADJUST = [" SO/ZD", " SW/SO", " SW/RO", " SD/GO1", " SW/AN", " HP7"]
+PIPES_TO_ADJUST = [" p23", " p129", " p159"]
 runepanet = r"D:\EPANET 2.2\runepanet.exe"
 
 
@@ -14,21 +13,16 @@ def update_epanet_file(filename, x):
     # read EPANET file
     with open(filename) as model_file:
         lines = model_file.readlines()
-    
-    # update junctions
-    #for j in range(junctions_start, junctions_end):
-    #    parts = lines[j].split()
-    #    lines[j] = lines[j].replace('\t' + parts[2], '\t'+str(x[j-5]))
 
-    # update pipes
     for i in range(pipes_start, pipes_end):
         parts = lines[i].split()
-        lines[i] = lines[i].replace('\t' + parts[5]+" ", '\t'+str(x[i-pipes_start]))
+        lines[i] = lines[i].replace(
+            "\t" + parts[5] + " ", "\t" + str(x[i - pipes_start]) + " "
+        )
 
     # write back to file
-    with open(filename, 'w') as model_file:
+    with open(filename, "w") as model_file:
         model_file.writelines(lines)
-
 
 
 def optimize_epanet(x):
@@ -41,41 +35,53 @@ def optimize_epanet(x):
 
     # Run EPANET and calculate error
     error = run_epanet_and_calculate_error(metric="mae")
-    
+    print(error)
+    with open("error.txt", mode="a") as e:
+        e.write(str(str(error) + "\n"))
+
     return error
 
+
 def add_report_section(original_model):
-    with open(original_model, 'r') as f:
+    with open(original_model, "r") as f:
         lines = f.readlines()
-        lines.reverse() 
+        lines.reverse()
         report_line = None
         end_line = None
         report_line_number = None
         end_line_number = None
-        nodes  = "NODES"
+        nodes = "NODES"
         pipes = "LINKS"
         for i, line in enumerate(lines):
-            if '[END]' in line and end_line is None:
+            if "[END]" in line and end_line is None:
                 end_line = line
-                end_line_number = len(lines) - i 
-            if '[REPORT]' in line and report_line is None:
+                end_line_number = len(lines) - i
+            if "[REPORT]" in line and report_line is None:
                 report_line = line
-                report_line_number = len(lines) - i  
+                report_line_number = len(lines) - i
             if report_line_number != None and end_line_number != None:
                 lines.reverse()
-                del lines[report_line_number:end_line_number+1]
-                for j in JUNCTIONS_TO_ADJUST:    
-                    nodes  += j
+                del lines[report_line_number : end_line_number + 1]
+                for j in JUNCTIONS_TO_ADJUST:
+                    nodes += j
                 for n in PIPES_TO_ADJUST:
                     pipes += n
-                text = [nodes,"\n",pipes,'\nFLOW YES','\nPRESSURE YES',"\n",'[END]']
-                for input in text:    
+                text = [
+                    nodes,
+                    "\n",
+                    pipes,
+                    "\nFLOW YES",
+                    "\nPRESSURE YES",
+                    "\n",
+                    "[END]",
+                ]
+                for input in text:
                     lines.append(input)
                 break
-                
-    with open(original_model, 'w') as f:
+
+    with open(original_model, "w") as f:
         f.writelines(lines)
-        
+
 
 def run_epanet_and_calculate_error(metric="mse"):
     """
@@ -85,46 +91,55 @@ def run_epanet_and_calculate_error(metric="mse"):
     :param metric: Name of the error metric ('mae' by default).
     :return: error -- The value of the calculated error metric.
     """
-    subprocess.run([runepanet, optimalize_model, optimalize_output],check=True)
+    subprocess.run([runepanet, optimalize_model, optimalize_output], check=True)
 
     # Read results from the report
     results_optimalize = get_simulation_results(optimalize_output)
-    results_true =  get_simulation_results(original_output)
-
+    results_true = get_simulation_results(original_output)
     error = calculate_error(results_true, results_optimalize, metric)
-    print(error)
-    if error < 0.05:
-        sys.exit()
     return error
 
 
-def main():
+def main(POPULATION, MAXITERATIONS, BOUNDS):
     """
     Main function to optimize EPANET model parameters.
     """
-    subprocess.run([runepanet, original_model, original_output],check=True)
+    subprocess.run([runepanet, original_model, original_output], check=True)
 
-
-    bounds = [(0,5)] * (pipes_end-pipes_start)
-    
-    differential_evolution(optimize_epanet, bounds,mutation=(0.5,1),recombination=0.7)
+    differential_evolution(
+        optimize_epanet,
+        BOUNDS,
+        mutation=(0.6, 0.9),
+        recombination=0.8,
+        popsize=POPULATION,
+        maxiter=MAXITERATIONS,
+    )
+    return BOUNDS
 
 
 # Copy model file
-original_model = 'epanet_model/model_original.inp'
+original_model = "epanet_model/model_original.inp"
 add_report_section(original_model)
 
 with open(original_model) as model_file:
     lines = model_file.readlines()
-    junctions_start = lines.index('[JUNCTIONS]\n') +2
-    junctions_end = lines.index('[RESERVOIRS]\n') -1
-    pipes_start = lines.index('[PIPES]\n') +2
-    pipes_end = lines.index('[PUMPS]\n') -1
+    junctions_start = lines.index("[JUNCTIONS]\n") + 2
+    junctions_end = lines.index("[RESERVOIRS]\n") - 1
+    pipes_start = lines.index("[PIPES]\n") + 2
+    pipes_end = lines.index("[PUMPS]\n") - 1
 
-optimalize_model = 'epanet_model/model_optimalize.inp'
+optimalize_model = "epanet_model/model_optimalize.inp"
 shutil.copyfile(original_model, optimalize_model)
-original_output = 'epanet_model/output_true_values.txt'
-optimalize_output = 'epanet_model/output_optimalize_values.txt'
+original_output = "epanet_model/output_true_values.txt"
+optimalize_output = "epanet_model/output_optimalize_values.txt"
+
+POPULATION = 40
+MAXITERATIONS = 10
+BOUNDS = [(0, 5)] * (pipes_end - pipes_start)
 
 if __name__ == "__main__":
-    main()
+    population_individuals = "population_individuals =  " + str(
+        (MAXITERATIONS + 1) * POPULATION * len(BOUNDS)
+    )
+    print(population_individuals)
+    main(POPULATION, MAXITERATIONS, BOUNDS)
